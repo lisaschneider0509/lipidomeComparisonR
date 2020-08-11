f370a7f49e0bdb3143c7b9d8b32b010886aedd5c
Lisa Schneider <lisa.schneider@stud.fh-campuswien.ac.at>
2020-08-11 22:12:45 +0200

    Added files of lipidomeComparison package

---
+++ R/data.R
@@ -0,0 +1,29 @@
+#' TraceFinder example data
+#'
+#' A dataset containing LC-MS data after compound identification with Thermo Fisher Tracefinder.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#' \item{price}{price, in US dollars}
+#' \item{carat}{weight of the diamond, in carats}
+#'   }
+#' }
+"tfData"
+
+#' The iris data set from R's data sets.
+#'
+#' Edgar Anderson's Iris Data. See
+#'
+#' @format A data frame with 150 cases (rows) and 5 variables (columns)
+#' \describe{
+#' \item{price}{price, in US dollars}
+#' \item{carat}{weight of the diamond, in carats}
+#'   }
+#' }
+"iris"
+
+#' Motor Trend Car Road Tests from R datasets
+"mtcars"
+
+#' Violent Crime Rates by US State from R datasets
+"USArrests"
--- R/lipidome_comparison_hypothesis_testing.R
+++ R/lipidome_comparison_hypothesis_testing.R
@@ -1,60 +1,49 @@
-## set ggplot theme
-my_theme <- theme_set(
-  theme_minimal() +
-    theme(plot.title = element_text(size=12, hjust = 0.5, family="AvantGarde"),
-          axis.text.x = element_text(size = 8, colour = "grey40", family="AvantGarde"),
-          axis.text.y = element_text(size = 8, colour = "grey40", family="AvantGarde"),
-          # axis.title = element_text(size = 10, colour = "grey40", family="AvantGarde"),
-          axis.title.x = element_text(size = 10, hjust = 0.5, colour = "grey40", family="AvantGarde"),
-          axis.title.y = element_text(size = 10, hjust = 0.5, colour = "grey40", family="AvantGarde"),
-          legend.text = element_text(size = 8, colour = "grey40", family="AvantGarde"),
-          legend.title = element_text(size = 10, colour = "grey40", family="AvantGarde"))
-)
-
 #' Perform one sample test by column
-#' 
-#' @description `p_values_by_column` takes a data frame and a grouping vector and returns a data frame with the p-values of a given method. 
-#' @details This function takes a data frame ans a grouping vector and performs a given hypothesis test on all numeric variables. The results are returned in a new data frame. 
-#' @param input_df data frame. Contains at least one numeric variable. 
+#'
+#' @description `p_values_by_column` takes a data frame and a grouping vector and returns a data frame with the p-values of a given method.
+#' @details This function takes a data frame ans a grouping vector and performs a given hypothesis test on all numeric variables. The results are returned in a new data frame.
+#' @param input_df data frame. Contains at least one numeric variable.
 #' @param group_vector vector. Vector containing the groups to be compared. group_vector = input_df$group (or other vector of length nrow(input_df))
 #' @param alternative string. Specifies the alternative hypothesis. c("two.sided" (default), "greater", "less")
 #' @param method function. Options: t.test (default), wilcox.test
 #' @param confidence_level numeric. Confidence level of the interval. Default = 0.95
-#' @example 
+#' @export
+#' @examples
 #' my_iris <- subset(x = iris, Species == "setosa" | Species == "versicolor")
 #' one_sample_test_by_col(my_iris, my_iris$Species)
 #' one_sample_test_by_col(my_iris, my_iris$Species, method = wilcox.test)
 one_sample_test_by_col <- function(input_df,
                             group_vector,
-                            alternative = "two.sided", 
+                            alternative = "two.sided",
                             method = t.test,
                             confidence_level = 0.95){
-  
-  p_values <- apply(select_if(input_df, is.numeric), 2, 
-                    function(x) method(x ~ group_vector, 
-                                       alternative = alternative, 
+
+  p_values <- apply(dplyr::select_if(input_df, is.numeric), 2,
+                    function(x) method(x ~ group_vector,
+                                       alternative = alternative,
                                        conf.level = confidence_level)$p.value)
   as.data.frame(p_values)
 }
 
 #' One way ANOVA by column
-#' 
-#' @description `one_way_anova_by_col` performs an ANOVA for each numeric column of a data frame and returns f-statistic and p-value in a data frame. 
-#' @param input_df data frame. Has multiple columns with numerical variables and at least one column with a factor variable. 
-#' @param factor_col string. Gives the name of the column, where the groups between which the anova is performed are stored. 
+#'
+#' @description `one_way_anova_by_col` performs an ANOVA for each numeric column of a data frame and returns f-statistic and p-value in a data frame.
+#' @param input_df data frame. Has multiple columns with numerical variables and at least one column with a factor variable.
+#' @param factor_col string. Gives the name of the column, where the groups between which the anova is performed are stored.
 #' @param print_all bool. Prints a summary of each anova. Default = FALSE
-#' @example 
+#' @export
+#' @examples
 #' one_way_anova_by_col(iris, "Species")
 #' one_way_anova_by_col(iris, "Species", print_all = TRUE)
 one_way_anova_by_col <- function(input_df, factor_col, print_all = FALSE){
   numeric_df <- data.frame(Group = input_df[[factor_col]])
-  numeric_df <- cbind(numeric_df, select_if(input_df, is.numeric))
-  
+  numeric_df <- cbind(numeric_df, dplyr::select_if(input_df, is.numeric))
+
   anova_df <- data.frame()
   for(i in 2:ncol(numeric_df)){
-    anova <- aov(numeric_df[[colnames(numeric_df)[i]]] ~ numeric_df$Group, 
+    anova <- stats::aov(numeric_df[[colnames(numeric_df)[i]]] ~ numeric_df$Group,
                  data = numeric_df)
-    
+
     a <- as.data.frame(unlist(summary(anova)))
     a <- as.data.frame(t(a))
     p_value <- a$`Pr(>F)1`
@@ -76,29 +65,30 @@ one_way_anova_by_col <- function(input_df, factor_col, print_all = FALSE){
   anova_df
 }
 
-#' ANOVA list 
-#' 
-#' @description `one_way_anova_by_col` performs an ANOVA for each numeric column of a data frame and returns f-statistic and p-value in a data frame. 
-#' @param input_df data frame. Has multiple columns with numerical variables and at least one column with a factor variable. 
-#' @param factor_col string. Gives the name of the column, where the groups between which the anova is performed are stored. 
+#' ANOVA list
+#'
+#' @description `one_way_anova_by_col` performs an ANOVA for each numeric column of a data frame and returns f-statistic and p-value in a data frame.
+#' @param input_df data frame. Has multiple columns with numerical variables and at least one column with a factor variable.
+#' @param factor_col string. Gives the name of the column, where the groups between which the anova is performed are stored.
 #' @param print_all bool. Prints a summary of each anova. Default = FALSE
-#' @example 
+#' @export
+#' @example
 #' x <- list_anova_by_col(iris, "Species")
 #' list_anova_by_col(iris, "Species", print_all = TRUE)
 list_anova_by_col <- function(input_df, factor_col, print_all = FALSE){
   numeric_df <- data.frame(Group = input_df[[factor_col]])
-  numeric_df <- cbind(numeric_df, select_if(input_df, is.numeric))
-  
+  numeric_df <- cbind(numeric_df, dplyr::select_if(input_df, is.numeric))
+
   anova_list <- list()
   for(i in 2:ncol(numeric_df)){
-    anova <- aov(numeric_df[[colnames(numeric_df)[i]]] ~ numeric_df$Group, 
+    anova <- stats::aov(numeric_df[[colnames(numeric_df)[i]]] ~ numeric_df$Group,
                  data = numeric_df)
-    
+
     anova_list[[i]] <- anova}
-  
+
   names(anova_list) <- colnames(numeric_df)
-  
-  
+
+
   anova_list
 }
 
@@ -106,38 +96,30 @@ list_anova_by_col <- function(input_df, factor_col, print_all = FALSE){
 
 
 #' Kruskal.test by column
-#' 
-#' @description `kruskal_test_by_col` performs a Kruskal-Wallis-Test for each numeric column of a data frame and returns a data frame. 
-#' @param input_df data frame. Has multiple columns with numerical variables and at least one column with a factor variable. 
-#' @param factor_col string. Gives the name of the column, where the groups between which the anova is performed are stored. 
+#'
+#' @description `kruskal_test_by_col` performs a Kruskal-Wallis-Test for each numeric column of a data frame and returns a data frame.
+#' @param input_df data frame. Has multiple columns with numerical variables and at least one column with a factor variable.
+#' @param factor_col string. Gives the name of the column, where the groups between which the anova is performed are stored.
 #' @param print_all bool. Prints a summary of each anova. Default = FALSE
-#' @example 
+#' @export
+#' @example
 #' kruskal_test_by_col(iris, "Species")
 #' kruskal_test_by_col(iris, "Species", print_all = TRUE)
 kruskal_test_by_col <- function(input_df, factor_col, print_all = FALSE){
   numeric_df <- data.frame(Group = input_df[[factor_col]])
-  numeric_df <- cbind(numeric_df, select_if(input_df, is.numeric))
-  
+  numeric_df <- cbind(numeric_df, dplyr::select_if(input_df, is.numeric))
+
   kruskal_df <- data.frame()
   for(i in 2:ncol(numeric_df)){
-    kruskal <- kruskal.test(numeric_df[[colnames(numeric_df)[i]]] ~ numeric_df$Group, 
+    kruskal <- stats::kruskal.test(numeric_df[[colnames(numeric_df)[i]]] ~ numeric_df$Group,
                  data = numeric_df)
-    
+
     statistic <- kruskal$statistic
     df <- kruskal$parameter
     p_value <- kruskal$p.value
     buffer <- cbind(statistic, df, p_value)
     kruskal_df <- rbind(kruskal_df, buffer)
-    
-    
-    # if(print_all){
-    #   cat("\n-----\n\n")
-    #   cat(colnames(numeric_df)[i])
-    #   cat("\n")
-    #   print(kruskal)
-    #   cat("\n")
-    # }
-    
+
   }
   rownames(kruskal_df) <- colnames(numeric_df[, -1])
   colnames(kruskal_df) <- c("kruskal-wallis_chi-squared", "df", "p_value")
@@ -146,39 +128,44 @@ kruskal_test_by_col <- function(input_df, factor_col, print_all = FALSE){
 
 
 #' Calculate log2 foldchange
-#' 
-#' @description `log2_foldchange` calculates the log2 foldchange for two groups of a given data frame. 
-#' @details This function takes a data frame and a grouping vector and calculates the log2 foldchange between two groups. 
-#' The results are returned in a new data frame. 
-#' @param input_df data frame. Contains at least one numeric variable. 
+#'
+#' @description `log2_foldchange` calculates the log2 foldchange for two groups of a given data frame.
+#' @details This function takes a data frame and a grouping vector and calculates the log2 foldchange between two groups.
+#' The results are returned in a new data frame.
+#' @param input_df data frame. Contains at least one numeric variable.
 #' @param group_vector vector. Vctor containing the grouping variabe. group_vector = input_df$group (or any vector of length nrow(input_df))
-#' @example 
+#' @param control_group string. Name of control group.
+#' @param test_group string. Name of test group.
+#' @export
+#' @example
 #' my_iris <- subset(x = iris, Species == "setosa" | Species == "versicolor")
 #' log2_foldchange(my_iris, my_iris$Species, control_group = "versicolor", test_group = "setosa")
-log2_foldchange <- function(input_df, group_vector, control_group, test_group){
-  log2_df <- select_if(input_df, is.numeric)
+log2_foldchange <- function(input_df,
+                            group_vector,
+                            control_group,
+                            test_group){
+  log2_df <- dplyr::select_if(input_df, is.numeric)
   log2_df$group <- group_vector
-  
-  means <- aggregate(select_if(log2_df, is.numeric), by = list(log2_df$group), FUN = mean)
+
+  means <- aggregate(dplyr::select_if(log2_df, is.numeric), by = list(log2_df$group), FUN = mean)
   rownames(means) <- means$Group.1
-  # means <- as.data.frame(select_if(means, is.numeric))
   means <- means[, -1]
-  
+
   control_group <- subset(means, rownames(means) == control_group)
   test_group <- subset(means, rownames(means) == test_group)
-  
+
   log2_foldchange <- vector()
   for(i in 1:ncol(means)){
     # if(control_group[i] < test_group[i]){
     #   log2_foldchange <- test_group[i]/control_group[i] * -1
     # }
     # else{
-    #   log2_foldchange <- control_group[i]/test_group[i] 
+    #   log2_foldchange <- control_group[i]/test_group[i]
     # }
-    # 
+    #
     log2_foldchange[i] <- control_group[i] - test_group[i]
   }
-  
+
   out_df <- t(as.data.frame(log2_foldchange))
   rownames(out_df) <- colnames(means)
   colnames(out_df) <- c("log2_foldchange")
@@ -186,43 +173,44 @@ log2_foldchange <- function(input_df, group_vector, control_group, test_group){
 }
 
 #' Volcano plot
-#' 
+#'
 #' @description `volcano_plot` prints a volcano plot using a data frame containing p-values and foldchanges
-#' @details This function takes a data frame with columns for p-values and (log2)foldchanges and returns a dotplot resembelling a volcano plot. 
+#' @details This function takes a data frame with columns for p-values and (log2)foldchanges and returns a dotplot resembelling a volcano plot.
 #' It uses the given significance and foldchange threshods to mark significantly up- and down-regulated elements by color and by displaying
-#' threshold lines. 
-#' @param input_df data frame. 
-#' @param foldchange_col vector. Column of input_df containing the foldchange values. Format: input_df$foldchange_col
-#' @param significance_col vector. Column of input_df containing the p-values. Format: input_df$significance_col
+#' threshold lines.
+#' @param volcano_df data frame.
+#' @param foldchange_col vector. Column of volcano_df containing the foldchange values. Format: volcano_df$foldchange_col
+#' @param significance_col vector. Column of volcano_df containing the p-values. Format: volcano_df$significance_col
 #' @param significance numeric. Value for the significance threshold. Default = 0.05.
 #' @param foldchange numeric. Vaue for the foldchange threshold. Default = #todo
 #' @param title string. Main title. Default = "Volcano plot"
 #' @param x_lab string. x-axix title. Default = "log2Fold"
 #' @param y_lab string. y-axis title- Default = "-log10(p-value)"
-#' @param labels boolean. Should the points over the threshold be labelled. Options: TRUE (default), FALSE
-#' If "none", the plot is either printed or saved to a variabe. 
-#' @example 
+#' @param labels logical. Should the points over the threshold be labelled. Options: TRUE (default), FALSE
+#' @param out_path string. Path to save plot to png. If out_path is empty, the plot is printed to the device. If "none", the plot is either printed or saved to a variabe.
+#' @export
+#' @example
 #' set.seed(100)
 #' fold_changes <- c(rnorm(2000, 0, 2))
 #' pvalues <- runif(n=2000, min=1e-50, max=.1)
 #' volcano_test <- as.data.frame(cbind(fold_changes, pvalues))
-#' vp <- volcano_plot(volcano_test, 
-#'              foldchange_col = volcano_test$fold_changes, 
-#'              significance_col = volcano_test$pvalues, 
+#' vp <- volcano_plot(volcano_test,
+#'              foldchange_col = volcano_test$fold_changes,
+#'              significance_col = volcano_test$pvalues,
 #'              significance = 0.001,
 #'              foldchange = 2)
-#' volcano_plot(volcano_test, 
-#'              foldchange_col = volcano_test$fold_changes, 
-#'              significance_col = volcano_test$pvalues, 
-#'              foldchange = 1, 
+#' volcano_plot(volcano_test,
+#'              foldchange_col = volcano_test$fold_changes,
+#'              significance_col = volcano_test$pvalues,
+#'              foldchange = 1,
 #'              labels =c(1:nrow(volcano_test)))
-#' \dontrun
-#' {dir.create(paste(getwd(), "/examples", sep = ""), showWarnings = FALSE)
+#' \dontrun{
+#' dir.create(paste(getwd(), "/examples", sep = ""), showWarnings = FALSE)
 #' dir <- paste(getwd(), "/examples/vp", sep = "")
-#' volcano_plot(volcano_test, 
-#'              foldchange_col = volcano_test$fold_changes, 
-#'              significance_col = volcano_test$pvalues, 
-#'              foldchange = 1, significance = 0.01, 
+#' volcano_plot(volcano_test,
+#'              foldchange_col = volcano_test$fold_changes,
+#'              significance_col = volcano_test$pvalues,
+#'              foldchange = 1, significance = 0.01,
 #'              out_path = dir)}
 volcano_plot <- function(volcano_df,
                          foldchange_col, significance_col,
@@ -232,9 +220,6 @@ volcano_plot <- function(volcano_df,
                          x_lab = "log2Foldchange", y_lab = "-log10(p-value)",
                          labels = TRUE,
                          out_path = "none"){
-
-  # volcano_df <- volcano_df[with(volcano_df, order(significance_col)), ]
-
   is_significant <- significance_col < significance
 
   threshold <- vector()
@@ -264,41 +249,49 @@ volcano_plot <- function(volcano_df,
   }
 
   x_limits <- max(-1*min(foldchange_col), max(foldchange_col), foldchange)
-  y_limits <- max(-1*log10(significance_col), -1*log10(significance)) 
-  mycolors <- viridis(n = 2, begin = 0, end = 0.9)
+  y_limits <- max(-1*log10(significance_col), -1*log10(significance))
+  mycolors <- viridis::viridis(n = 2, begin = 0, end = 0.9)
 
-  volcano <- ggplot(data = volcano_df,
-                    aes(x = foldchange_col, y = -1*log10(significance_col), text = rownames)) +
-    geom_point(aes(color = as.factor(threshold)), shape = 20) +
-    geom_hline(yintercept = -1*log10(significance),
+  volcano <- ggplot2::ggplot(data = volcano_df,
+                             ggplot2::aes(x = foldchange_col, y = -1*log10(significance_col), text = rownames)) +
+    ggplot2::geom_point(aes(color = as.factor(threshold)), shape = 20) +
+    ggplot2::geom_hline(yintercept = -1*log10(significance),
                linetype = "dashed",
                colour = "grey60") +
-    geom_vline(xintercept = -1*foldchange,
+    ggplot2::geom_vline(xintercept = -1*foldchange,
                linetype = "dashed",
                colour = "grey60") +
-    geom_vline(xintercept = foldchange,
+    ggplot2::geom_vline(xintercept = foldchange,
                linetype = "dashed",
                colour = "grey60") +
-    geom_text_repel(aes(x = foldchange_col,
+    ggrepel::geom_text_repel(aes(x = foldchange_col,
                         y = -1*log10(significance_col),
                         label = `mylabel`),
                     size = 2) +
-    labs(title = title
+    ggplot2::labs(title = title
          # subtitle = paste(foldchange, "x log2-Foldchange, Significance = ", significance, sep = "")
          ) +
-    xlab(x_lab) + ylab(y_lab) +
-    scale_x_continuous(limits = c(-1*x_limits, x_limits)) +
-    scale_y_continuous(limits = c(0, y_limits)) +
-    scale_color_manual(name = "Threshold",
+    ggplot2::xlab(x_lab) + ylab(y_lab) +
+    ggplot2::scale_x_continuous(limits = c(-1*x_limits, x_limits)) +
+    ggplot2::scale_y_continuous(limits = c(0, y_limits)) +
+    ggplot2::scale_color_manual(name = "Threshold",
                        values = c("up" = mycolors[1], "down" = mycolors[2], "not significant" = "grey60")
                        # labels = c("down" = "Down", "non significant", "Up")
                        ) +
-    theme(legend.position = "bottom")
+    ggplot2::theme_minimal() +
+    ggplot2::theme(plot.title = element_text(size=12, hjust = 0.5),
+          axis.text.x = ggplot2::element_text(size = 8),
+          # axis.title = ggplot2::element_text(size = 10),
+          axis.title.x = ggplot2::element_text(size = 10, hjust = 0.5),
+          axis.title.y = ggplot2::element_text(size = 10, hjust = 0.5),
+          legend.text = ggplot2::element_text(size = 8),
+          legend.title = ggplot2::element_text(size = 10),
+          legend.position = "bottom")
 
 
   if(out_path != "none"){
     print(paste("Saving plot to ", out_path, "_volcano.png", sep = ""))
-    ggsave(paste(out_path, "_volcano.png", sep = ""),
+    ggplot2::ggsave(paste(out_path, "_volcano.png", sep = ""),
            plot = volcano)
   }
   volcano
